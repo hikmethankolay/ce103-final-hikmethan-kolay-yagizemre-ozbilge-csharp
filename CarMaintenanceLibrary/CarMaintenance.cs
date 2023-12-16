2353d1499e07346c5bd8c53cba9414cf419b3bc8
Yağız Emre <yagizozbilge.@gmail.com>
2023-12-16 19:38:25 +0300

    Bug fixes

--- CarMaintenanceLibrary/CarMaintenance.cs
+++ CarMaintenanceLibrary/CarMaintenance.cs
@@ -1,7 +1,7 @@
 ﻿namespace CarMaintenanceLibrary {
 /// \brief A class for performing basic arithmetic operations.
 public class CarMaintenance {
-        /**
+ /**
  * @brief This function register records to expense_logging_records.bin.
  *
  *
@@ -40,6 +40,47 @@ public class CarMaintenance {
   }
         return 0;
 }
-  
+
+ /**
+ * @brief This function register records to expense_logging_records.bin.
+ *
+ *
+ * @return 0 on success.
+ * @return -1 on fail.
+ */
+ static int EditExpsenseRecord(string filename,int linenumbertoEdit, string carModel, string expenseDate, string expenseType, int expense)
+  {
+  string record;
+
+            if (carModel == "None" && expenseDate == "None" && linenumbertoEdit == 0 && expense == 1 && expenseType == "None") {
+            Console.WriteLine("Which do you want to edit");
+            linenumbertoEdit = int.Parse(Console.ReadLine());
+
+            if (!int.TryParse(Console.ReadLine(), out linenumbertoEdit)) {
+                Console.WriteLine("Please use an integer");
+                return -1;
+            }
+            Console.WriteLine("What is the model of the car?");
+            carModel = Console.ReadLine();
+            Console.WriteLine("What is the expense date?");
+            expenseDate = Console.ReadLine();
+           Console.WriteLine("What is the expenseType");
+           expenseType = Console.ReadLine();
+
+            if (!int.TryParse(Console.Readline(), out expense) {
+                Console.WriteLine("Please use an integer");
+                return -1;
+            }
+      }
+
+      record = $"{carModel} {expenseDate} {expenseType} {expense} ";
+
+        if (FileEdit(filename, linenumbertoEdit, record) == 0) {
+            return 0;
+        } else {
+            return -1;
+        }
+ }
+
 
 
